27ed9ea727aa095dac36022bc492fbdf075e8693
Greg Smirnoff <gregsm1988@gmail.com>
2013-12-18 18:46:13 +0400

    initial commit

---
+++ js/charts.js
@@ -0,0 +1,141 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: GSmirnoff
+ * Date: 18.12.13
+ * Time: 13:33
+ * To change this template use File | Settings | File Templates.
+ */
+var charts = (function () {
+    var drawChart = function (csvlocation, placeholder) {
+        d3.text(csvlocation, function (csvText) {
+            var parsed = d3.csv.parseRows(csvText.replace(/\s*;\s*/g, ","));
+
+            //in 1st cell there is name of chart and names of cells
+            if (parsed.length == 0) return null;
+
+            var headers = parsed.shift();
+            var colHeaders = headers.splice(headers.length + 1 - parsed[0].length);
+            var legend = $.map(parsed, function (elem) {
+                return elem.shift()
+            });
+            for (var i = 0; i < colHeaders.length; i++) colHeaders[i] = messages.get(colHeaders[i]);
+            var header = messages.get(headers[0]);
+
+            var chartData = [];
+            for (var i = 0; i < legend.length; i++) {
+                var paramData = {
+                    key: legend[i],
+                    values: []
+                }
+                for (var j = 0; j < parsed[i].length; j++) {
+                    paramData.values.push({x: colHeaders[j], y: parseFloat(parsed[i][j])});
+                }
+                chartData.push(paramData);
+            }
+
+
+            //then draw chart itself
+            var margin = {top: 20, right: 10, bottom: 20, left: 25},
+                width = 330 - margin.left - margin.right,
+                height = 200 - margin.top - margin.bottom,
+                keys = chartData[0].values.map(function (item) {
+                    return item.x
+                }),
+                stack = d3.layout.stack().values(function (d) {
+                    return d.values;
+                }),
+                layers = stack(chartData),
+                yMax = d3.max(layers, function (layer) {
+                    return d3.max(layer.values, function (d) {
+                        return d.y0 + d.y;
+                    });
+                })
+
+            //Calulate totals for each x value in the domain
+            var totals = {};
+            chartData.forEach(function (series) {
+                series.values.forEach(function (item) {
+                    totals[item.x] = (totals[item.x] || 0 ) + item.y
+                })
+            })
+
+            var x = d3.scale.ordinal()
+                .domain(keys)
+                .rangeRoundBands([0, width], .22);
+
+            var y = d3.scale.linear()
+                .domain([0, yMax])
+                .range([height, 0]);
+
+            var color = d3.scale.ordinal()
+                .domain([0, chartData.length - 1])
+                .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .tickSize(1)
+                .tickPadding(2)
+                .orient("left");
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .tickSize(1)
+                .tickPadding(6)
+                .orient("bottom");
+
+            var svg = d3.select(placeholder).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var layer = svg.selectAll(".layer")
+                .data(layers)
+                .enter().append("g")
+                .attr("class", "layer")
+                .style("fill", function (d, i) {
+                    return color(i);
+                });
+
+            layer.selectAll("rect")
+                .data(function (d) {
+                    return d.values;
+                })
+                .enter().append("rect")
+                //.attr("fill-opacity", .5)
+                .attr("stroke", "#000")
+                .attr("width", x.rangeBand())
+                .attr("x", function (d) {
+                    return x(d.x);
+                })
+                .attr("y", function (d) {
+                    return y(d.y0 + d.y);
+                })
+                .attr("height", function (d) {
+                    return y(d.y0) - y(d.y0 + d.y);
+                });
+
+            svg.selectAll('.tick').selectAll('text')
+                .style('font-size', 10);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
+        })
+    }
+    $(document).ready(function () {
+        var csvs = ['data/solod_2013.csv', 'data/syrop_total.csv'];
+        var placeHolders = $('.chart');
+        for (var i = 0; i < placeHolders.length; i++) {
+            if (csvs[i] != null) {
+                var placeHolder = $(placeHolders.get(i)).find('.grapholder-inner').empty();
+                drawChart(csvs[i], placeHolder[0]);
+            }
+        }
+    })
+})()
---
+++ js/messages.js
@@ -0,0 +1,40 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: GSmirnoff
+ * Date: 18.12.13
+ * Time: 14:31
+ * To change this template use File | Settings | File Templates.
+ */
+var messages = {
+    jan:'Январь',
+    feb:'Февраль',
+    mar:'Март',
+    apr:'Апрель',
+    may:'Май',
+    jun:'Июнь',
+    jul:'Июль',
+    aug:'Август',
+    sep:'Сентябрь',
+    oct:'Октябрь',
+    nov:'Ноябрь',
+    dec:'Декабрь',
+    tula:'Тула',
+    yaroslavl:'Ярославль',
+    totalForCompany:'Итого по компании',
+    volumes:'Объемы, тыс. кг',
+    lossMalt:'Потери экстракта при производстве солода, %',
+    stageLosses:'Стадии потерь',
+    barleyTransportationLosses:'Потери при транспортировке ячменя,%',
+    barleySortingLosses:'Потери при сортировке ячменя,%',
+    maltProductionLosses:'Потери при производстве солода,%',
+    cerealTransportLosses:'Потери при внутризаводской транспортировке зернопродуктов, %',
+    totalExtractLossesByFactory:'Итого потери экстракта по заводу',
+    targetExtractLosses:'Цель потерь экстракта',
+    avgByYearL:'Среднее за год',
+    totalExtractLossesByMaltFactory:'Общие потери экстракта по солодовенному цеху,%',
+    extractLossesStatsBaltic:'Статистика потерь экстракта по стадиям, завод "Балтика-СПб"',
+
+    get: function (m) {
+        return this[m]?this[m]:m;
+    }
+}
