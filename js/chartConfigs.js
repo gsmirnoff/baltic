99c22a8d6d9585c04d29a478749bff061dd1d732
Greg Smirnoff <gregsm1988@gmail.com>
2013-12-19 14:53:24 +0400

    new (1.1) chart

--- js/chartConfigs.js
+++ js/chartConfigs.js
@@ -9,6 +9,12 @@ var chartConfigs = {
         colorSet:['#e0bcbc', '#d19392', '#aa4643', '#903a38'],
         tips:['y']
     },
+    solodBaltic:{
+        csv:'data/solod_baltic.csv',
+        colorSet:['#e0bcbc', '#d19392', '#aa4643', '#903a38'],
+        tips:['y'],
+        hideX:true
+    },
     syropYear:{
         csv:'data/syrop_total.csv',
         colorSet:['#4f81bd'],
