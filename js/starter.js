30a1ce2207d0b825f6c693d2b21b47c2d5ec5b05
SergeySukhanov <sergey.n.sukhanov@gmail.com>
2013-12-19 11:59:56 +0400

    added main controller and tools

---
+++ js/starter.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: SNSukhanov
+ * Date: 19.12.13
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var BALTIC = (function(m){
+
+
+    m.init = function() {
+       // условия для импровизированных ролей
+    }
+    return m;
+}(BALTIC || {}));
+
+
+$(document).ready(function(){
+   BALTIC.init();
+});
+
---
+++ js/tools/BalticTools.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: SNSukhanov
+ * Date: 19.12.13
+ * Time: 11:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+BALTIC.ToolsController = (function(module){
+    var view = {};
+
+
+
+    view.init = function(){
+
+    };
+
+    return view;
+}(Zero));
