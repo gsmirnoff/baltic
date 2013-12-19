77026ec6a5bde0a0693bcd3ec343c73051f45129
Greg Smirnoff <gregsm1988@gmail.com>
2013-12-19 16:34:55 +0400

    chart comments

--- js/chartConfigs.js
+++ js/chartConfigs.js
@@ -1,24 +1,31 @@
 var chartConfigs = {
-    solodYear:{
+    solodYear:{//1.3
         csv:'data/solod_2013.csv',
         colorSet:['#e0bcbc', '#d19392', '#aa4643', '#903a38'],
         tips:['y']
     },
-    solodNov:{
+    solodNov:{//1.2
         csv:'data/solod_nov.csv',
         colorSet:['#e0bcbc', '#d19392', '#aa4643', '#903a38'],
         tips:['y']
     },
-    solodBaltic:{
+    solodBaltic:{//1.1
         csv:'data/solod_baltic.csv',
         colorSet:['#e0bcbc', '#d19392', '#aa4643', '#903a38'],
-        tips:['y'],
+        tips:['x', 'y'],
         hideX:true
     },
-    syropYear:{
+    syropYear:{//2.1
         csv:'data/syrop_total.csv',
         colorSet:['#4f81bd'],
         tips:['x', 'y'],
         hideX:true
+    },
+    glassYear:{//don't use it for now
+        csv:'data/glass_2013.csv',
+        colorSet:['#4f81bd'],
+        tips:['x', 'y'],
+        hideX:true,
+        withLineChart:true
     }
 }
